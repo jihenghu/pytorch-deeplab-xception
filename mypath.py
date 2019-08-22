2f8cfc38143e87bfa96b9d708b098f94ebf06d02
Lingcao Huang <huanglingcao@gmail.com>
2019-08-22 21:52:58 +0800

    user home

--- mypath.py
+++ mypath.py
@@ -1,11 +1,13 @@
+import os
+HOME = os.path.expanduser('~')
 class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'pascal':
             # return '/path/to/datasets/VOCdevkit/VOC2012/'  # folder that contains VOCdevkit/.
-            return '/home/hlc/Data/experiment/darknet_expr/VOCdevkit/VOC2012'  # VOC2012 on Cryo06
+            return HOME+'/Data/experiment/darknet_expr/VOCdevkit/VOC2012'  # VOC2012 on Cryo06
         elif dataset == 'sbd':
-            return '/home/hlc/Data/experiment/darknet_expr/benchmark_RELEASE/'  # folder that contains dataset/.
+            return HOME +'/Data/experiment/darknet_expr/benchmark_RELEASE/'  # folder that contains dataset/.
         elif dataset == 'cityscapes':
             return '/path/to/datasets/cityscapes/'     # foler that contains leftImg8bit/
         elif dataset == 'coco':
