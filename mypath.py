c9b24ef82bd00a7ffe8b985c8804b67cc36570ac
Lingcao Huang <huanglingcao@gmail.com>
2019-08-22 21:49:15 +0800

    add sd path

--- mypath.py
+++ mypath.py
@@ -5,7 +5,7 @@ class Path(object):
             # return '/path/to/datasets/VOCdevkit/VOC2012/'  # folder that contains VOCdevkit/.
             return '/home/hlc/Data/experiment/darknet_expr/VOCdevkit/VOC2012'  # VOC2012 on Cryo06
         elif dataset == 'sbd':
-            return '/path/to/datasets/benchmark_RELEASE/'  # folder that contains dataset/.
+            return '/home/hlc/Data/experiment/darknet_expr/benchmark_RELEASE/'  # folder that contains dataset/.
         elif dataset == 'cityscapes':
             return '/path/to/datasets/cityscapes/'     # foler that contains leftImg8bit/
         elif dataset == 'coco':
