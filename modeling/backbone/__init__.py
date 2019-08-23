82a6cc776c8c31e96521268805ad9dc1a65812b1
Lingcao Huang <huanglingcao@gmail.com>
2019-08-23 14:52:23 +0800

    ignore ssl

--- modeling/backbone/__init__.py
+++ modeling/backbone/__init__.py
@@ -1,5 +1,9 @@
 from modeling.backbone import resnet, xception, drn, mobilenet
 
+# don't check CERTIFICATE, this cause issue on ITSC services
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 def build_backbone(backbone, output_stride, BatchNorm,pretrained=True):
     if backbone == 'resnet':
         return resnet.ResNet101(output_stride, BatchNorm,pretrained=pretrained)
