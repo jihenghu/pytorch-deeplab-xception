31fcbabeb7f7e6354417f0f7381b1fd02bda813b
Lingcao Huang <huanglingcao@gmail.com>
2019-08-23 08:48:48 +0800

    used bsd data

--- train_voc.sh
+++ train_voc.sh
@@ -14,7 +14,7 @@
 # test training on Cryo06
 export CUDA_VISIBLE_DEVICES=1
 python train.py --backbone resnet --lr 0.007 --workers 4 \
- --epochs 50 --batch-size 8 --gpu-ids 0 \
+--use-sbd --epochs 50 --batch-size 8 --gpu-ids 0 \
 --checkname deeplab-resnet --eval-interval 1 --dataset pascal
 
 
