ffc22f5da59a6ec66086d7df0e58489a75fadfbd
Lingcao Huang <huanglingcao@gmail.com>
2019-08-23 09:07:02 +0800

    added setting

--- train_voc_chpc.sh
+++ train_voc_chpc.sh
@@ -5,6 +5,27 @@
 
 #cp /home/hlc/Data/deeplab/v3+/pytorch-deeplab-xception/deeplab-resnet.pth.tar deeplab-resnet.pth.tar
 
+# use python 3 with pytorch 1.2
+export PATH=~/programs/anaconda3/bin:$PATH
+
+deeplab_dir=~/codes/PycharmProjects/pytorch-deeplab-xception
+
+cd ${deeplab_dir}
+git pull
+cd -
+
+para_file=para.ini
+
+if [ ! -f $para_file ]; then
+   echo "File ${para_file} not exists in current folder: ${PWD}"
+   exit 1
+fi
+
+# copy pretrained model
+if [ ! -f deeplab-resnet.pth.tar ]; then
+   echo "Copy pretrained model"
+   cp ~/Data/deeplab/v3+/pytorch-deeplab-xception/deeplab-resnet.pth.tar deeplab-resnet.pth.tar
+fi
 
 # test training on ITSC services with 8 GPU
 #export CUDA_VISIBLE_DEVICES=1
