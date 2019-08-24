e04e5922f33a1089bf96c1c9ce102d799d7128be
Lingcao Huang <huanglingcao@gmail.com>
2019-08-24 15:28:00 +0800

    allow resume training

--- train_voc_chpc.sh
+++ train_voc_chpc.sh
@@ -10,6 +10,7 @@ set -eE -o functrace
 export PATH=~/programs/anaconda3/bin:$PATH
 
 deeplab_dir=~/codes/PycharmProjects/yghlc_pytorch-deeplab-xception
+para_py=~/codes/PycharmProjects/DeeplabforRS/parameters.py
 
 
 para_file=para.ini
@@ -25,11 +26,25 @@ if [ ! -f deeplab-resnet.pth.tar ]; then
    cp ~/Data/deeplab/v3+/pytorch-deeplab-xception/deeplab-resnet.pth.tar deeplab-resnet.pth.tar
 fi
 
+expr_name=$(python2 ${para_py} -p ${para_file} expr_name)
+
 # test training on ITSC services with 8 GPU
 #export CUDA_VISIBLE_DEVICES=1
-python ${deeplab_dir}/train.py --backbone resnet --lr 0.007 --workers 4 \
---use-sbd --epochs 50 --batch-size 32 --gpu-ids 0,1,2,3,4,5,6,7 \
---checkname deeplab-resnet --eval-interval 1 --dataset pascal
+
+if [ ! -f ${expr_name}/checkpoint.pth.tar ]; then
+
+    # resume training
+    python ${deeplab_dir}/train.py --backbone resnet --lr 0.007 --workers 4 \
+    --use-sbd --epochs 50 --batch-size 32 --gpu-ids 0,1,2,3,4,5,6,7 \
+    --checkname deeplab-resnet --eval-interval 1 --dataset pascal \
+    --resume ${expr_name}/checkpoint.pth.tar
+
+else
+    python ${deeplab_dir}/train.py --backbone resnet --lr 0.007 --workers 4 \
+    --use-sbd --epochs 50 --batch-size 32 --gpu-ids 0,1,2,3,4,5,6,7 \
+    --checkname deeplab-resnet --eval-interval 1 --dataset pascal
+
+fi
 
 
 #  False
